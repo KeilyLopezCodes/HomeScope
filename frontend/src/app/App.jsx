9468e72280a52fbf28f695011013135949516e49
Astrid Polanco <apenap1@miumg.edu.gt>
2026-09-24 21:18:40 -0600

    Reestructurar carpetas según arquitectura

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,7 +2,7 @@
 function App() {
 
   return (
-  <div style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "4rem" }}>
+  <div className="text-red-500" style={{ fontFamily: "sans-serif", textAlign: "center", marginTop: "4rem" }}>
   <h1>🏠 HomeScope</h1>
   <p>Hola mundo: la implementación inicial del frontend está en funcionamiento.</p>
   </div>
